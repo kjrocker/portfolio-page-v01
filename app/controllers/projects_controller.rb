a04b5266536b5635e6a82fb2e5e9892e649d2231
Kevin Rocker <kevin.j.rocker@gmail.com>
2016-08-26 17:30:00 -0400

    Add authentication, index action to controller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,15 @@
 class ProjectsController < ApplicationController
+  before_action :authenticate_user!, except: [:show, :index]
+  before_action :set_project, except: [:index, :new]
+
+  def index
+    @projects = Project.all
+  end
+
+  private
+
+  def set_project
+    @project = Project.find(params[:id])
+  end
+
 end
