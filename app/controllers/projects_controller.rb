1e5ad73ebad5a7aa0af3685dfe9186860cf06369
Kevin Rocker <kevin.j.rocker@gmail.com>
2016-08-26 18:24:57 -0400

    Add new action

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,9 +1,13 @@
 class ProjectsController < ApplicationController
-  before_action :authenticate_user!, except: [:show, :index]
+  before_action :authenticate_admin!, except: [:show, :index]
   before_action :set_project, except: [:index, :new]
 
   def index
     @projects = Project.all
+  end
+
+  def new
+    @project = Project.new
   end
 
   private
