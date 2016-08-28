8e31448b108d8bdc392650d1dace6aa0cf39785e
Kevin Rocker <kevin.j.rocker@gmail.com>
2016-08-28 15:14:39 -0400

    Index only active projects unless logged in

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -3,7 +3,11 @@ class ProjectsController < ApplicationController
   before_action :set_project, only: [:show, :edit, :update, :destroy]
 
   def index
-    @projects = Project.all
+    if admin_signed_in?
+      @projects = Project.all
+    else
+      @projects = Project.where(active: true)
+    end
   end
 
   def new
