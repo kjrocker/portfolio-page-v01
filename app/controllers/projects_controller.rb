7f6adfa80dcb875c7d6b17fbb39efc6a93f4084f
Kevin Rocker <kevin.j.rocker@gmail.com>
2016-09-01 00:12:59 -0400

    Formatting changes, removing show action from projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,5 @@
 class ProjectsController < ApplicationController
-  before_action :authenticate_admin!, except: [:show, :index]
+  before_action :authenticate_admin!, except: [:index]
   before_action :set_project, only: [:show, :edit, :update, :destroy]
 
   def index
